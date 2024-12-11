badfa25d90a8b06c1cb0244025dd25a4a4b715f3
ThanosSkentzos <kalligerist@gmail.com>
2024-12-12 00:56:09 +0100

    get data

--- data.py
+++ data.py
@@ -4,11 +4,16 @@ import graph_tool.all as gt
 import networkx as nx
 
 
-def get_terror():
+def get_terror() -> nx.Graph:
     g = gt.collection.ns["terrorists_911"]
     edgelist = [(int(e.source()), int(e.target())) for e in list(g.edges())]
     G = nx.from_edgelist(edgelist)
     return G
 
 
+def get_streets() -> nx.Graph:
+    g = gt.collection.ns["urban_streets/ahmedabad"]
+    edgelist = [(int(e.source()), int(e.target())) for e in list(g.edges())]
+    G = nx.from_edgelist(edgelist)
+    return G
 # %%
